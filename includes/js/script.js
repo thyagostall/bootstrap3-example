f5ce65f8a17fddc97be86a1f1d16a25e9765eafc
thyagostall <thstall@gmail.com>
2015-07-23 11:53:08 -0400

    Modal window created

--- includes/js/script.js
+++ includes/js/script.js
@@ -16,4 +16,12 @@ $(function() {
         $('#success-alert').slideDown();
     });
 
+    $('a.pop').click(function(e) {
+        e.preventDefault();
+    });
+
+    $('a.pop').popover();
+
+    $('[rel="tooltip"]').tooltip();
+
 });
